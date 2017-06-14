645aaa3ba19943fa2e26842dac73593f22b97e49
jrwhite <jrwhite20@gmail.com>
2017-06-14 15:12:03 -0500

    added bit-bang

--- FIRMWARE/bit-bang.h
+++ FIRMWARE/bit-bang.h
@@ -11,5 +11,4 @@ void writeBit(void); // write from the write_buffer
 void startRead(pin_t *pin, uint8_t num_bits_to_read); // start reading from an input pin
 void endRead(pin_t *pin); // stop reading from an input pin
 
-
 #endif
